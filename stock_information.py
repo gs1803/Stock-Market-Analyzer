933f00866460109df9338b4603514e1fb06f6617
gs1803 <98928448+gs1803@users.noreply.github.com>
2022-12-25 21:18:13 -0600

    Update stock_information.py

--- stock_information.py
+++ stock_information.py
@@ -3,51 +3,52 @@ import pandas as pd
 from tabulate import tabulate
 
 class StockInformation:
-    def stock_recommendations(stock) -> None:
+    def __init__(self, stock) -> None:
+        self.stock = yf.Ticker(stock)
+
+    def stock_recommendations(self) -> None:
         try:
-            stock = yf.Ticker(stock)
-            analystRecom = stock.recommendations
+            analystRecom = self.stock.recommendations
             del analystRecom["From Grade"]
             headers_row = ['Date & Time', 'Firm', 'To Grade', 'Action']
             print(tabulate(analystRecom.tail(), headers = headers_row, tablefmt = 'psql'))
         except KeyError:
             print(" ")
 
-    def stock_dividends(stock) -> None:
+    def stock_dividends(self) -> None:
         try:
-            stock = yf.Ticker(stock)
-            df_StockDiv = pd.DataFrame(stock.dividends)
-            df_StockDiv.reset_index(inplace=True)
+            df_StockDiv = pd.DataFrame(self.stock.dividends)
+            df_StockDiv.reset_index(inplace = True)
             df_StockDiv['Date'] = pd.to_datetime(df_StockDiv['Date']).dt.date
             headers_row = ['Date', 'Dividends']
             print(tabulate(df_StockDiv.tail(), headers = headers_row, tablefmt = 'psql', showindex = False))
         except KeyError:
             print(" ")
 
-    def stock_splits(stock) -> None:
+    def stock_splits(self) -> None:
         try:
-            stock = yf.Ticker(stock)
-            df_StockSpl = pd.DataFrame(stock.splits)
-            df_StockSpl.reset_index(inplace=True)
+            df_StockSpl = pd.DataFrame(self.stock.splits)
+            df_StockSpl.reset_index(inplace = True)
             df_StockSpl['Date'] = pd.to_datetime(df_StockSpl['Date']).dt.date
             headers_row = ['Date', 'Splits']
             print(tabulate(df_StockSpl.tail(), headers = headers_row, tablefmt = 'psql', showindex = False))
         except KeyError:
             print(" ")
 
-def div_spl_chooser(stock) -> None:
-    while True:
-        print("+------------------+")
-        print("|d: Dividends      |")
-        print("|s: Splits         |")
-        print("|q: Quit to Menu   |")
-        print("+------------------+")
-        divSplOption = input("Select an option: ")
-        if divSplOption == 'd':
-            StockInformation.stock_dividends(stock)
-        elif divSplOption == 's':
-            StockInformation.stock_splits(stock)
-        elif divSplOption == 'q':
-            break
-        else:
-            print("Enter a valid option.")
+    def div_spl_chooser(self) -> None:
+        while True:
+            print("+------------------+")
+            print("|d: Dividends      |")
+            print("|s: Splits         |")
+            print("|q: Quit to Menu   |")
+            print("+------------------+")
+            divSplOption = input("Select an option: ")
+
+            if divSplOption == 'd':
+                StockInformation.stock_dividends(self)
+            elif divSplOption == 's':
+                StockInformation.stock_splits(self)
+            elif divSplOption == 'q':
+                break
+            else:
+                print("Enter a valid option.")
